4c5d7025dda4d0152bc287458051da69792880b7
Marcin Sztolcman <marcin@urzenia.net>
2013-10-03 13:21:28 +0200

    added module docstring

--- subst.py
+++ subst.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+""" subst - Replace PATTERN with REPLACE in many files.
+    http://mysz.github.io/subst
+    Author: Marcin Sztolcman (marcin@urzenia.net)
+
+    Get help with: subst --help
+    Information about version: subst --version
+"""
+
 from __future__ import print_function, unicode_literals
 
 import argparse
