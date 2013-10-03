a5671e1861cd8f4a1192899cbb11f73bc2116d8b
Marcin Sztolcman <marcin@urzenia.net>
2013-10-03 16:00:59 +0200

    tests for subst.errmsg

---
+++ test/test_errmsg.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python -tt
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
+import os, os.path
+import sys
+import re
+from pprint import pprint, pformat
+
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+from test_manager import *
+import subst
+
+class TestErrMsg(unittest.TestCase):
+    def test_simple(self):
+        old_stderr = sys.stderr
+        sys.stderr = StringIO()
+
+        data_in = "ala ma kota"
+        end = "\n"
+        subst.errmsg(data_in)
+
+        data = sys.stderr.getvalue()
+        sys.stderr = old_stderr
+
+        self.assertEqual(data, data_in + end)
+
+    def test_indent(self):
+        old_stderr = sys.stderr
+        sys.stderr = StringIO()
+
+        data_in = "ala ma kota"
+        end = "\n"
+        indent = 1
+        subst.errmsg(data_in, indent)
+
+        data = sys.stderr.getvalue()
+        sys.stderr = old_stderr
+
+        self.assertEqual(data, ' ' * indent * 4 + data_in + end)
+
+        old_stderr = sys.stderr
+        sys.stderr = StringIO()
+
+        data_in = "ala ma kota"
+        indent = 2
+        subst.errmsg(data_in, indent)
+
+        data = sys.stderr.getvalue()
+        sys.stderr = old_stderr
+
+        self.assertEqual(data, ' ' * indent * 4 + data_in + "\n")
+
+    def test_end(self):
+        old_stderr = sys.stderr
+        sys.stderr = StringIO()
+
+        data_in = "ala ma kota"
+        end = "-"
+        subst.errmsg(data_in, end=end)
+
+        data = sys.stderr.getvalue()
+        sys.stderr = old_stderr
+
+        self.assertEqual(data, data_in + end)
+
+        old_stderr = sys.stderr
+        sys.stderr = StringIO()
+
+        data_in = "ala ma kota"
+        end = "__END__"
+        subst.errmsg(data_in, end=end)
+
+        data = sys.stderr.getvalue()
+        sys.stderr = old_stderr
+
+        self.assertEqual(data, data_in + end)
