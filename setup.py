a234ed27c24088dcc46018e70110dd928117d2eb
Marcin Sztolcman <marcin@urzenia.net>
2014-11-14 21:57:12 +0100

    package configuration for pypi

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+BASE_DIR = path.abspath(path.dirname(__file__))
+
+with open(path.join(BASE_DIR, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='subst',
+    version='0.3.1',
+    description='`subst` is simple utility to replace one string into another in given list of files.',
+    long_description=long_description,
+    url='http://mysz.github.io/subst/',
+    author='Marcin Sztolcman',
+    author_email='marcin@urzenia.net',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='sed text processing',
+
+    entry_points={
+        'console_scripts': [
+            'subst=subst:main',
+        ],
+    },
+)
+
